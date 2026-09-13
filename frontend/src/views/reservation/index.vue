388d4599fc740d0b1009adc6c6697a239b738ed8
minih11 <2162245672@qq.com>
2026-09-13 23:28:21 +0800

    前端修改

--- frontend/src/views/reservation/index.vue
+++ frontend/src/views/reservation/index.vue
@@ -18,8 +18,9 @@
     <el-card>
       <el-table :data="tableData" v-loading="loading" border stripe>
         <el-table-column type="index" label="序号" width="60" />
-        <el-table-column prop="reservationNo" label="预约编号" width="180" />
-        <el-table-column prop="equipmentId" label="设备ID" width="100" />
+        <el-table-column prop="reservationNo" label="预约编号" width="150" />
+        <el-table-column prop="equipmentId" label="设备ID" width="70" />
+        <el-table-column prop="userName" label="申请者名字" width="130"></el-table-column>
         <el-table-column prop="startTime" label="开始时间" width="180" />
         <el-table-column prop="endTime" label="结束时间" width="180" />
         <el-table-column prop="purpose" label="使用目的" min-width="150" />
